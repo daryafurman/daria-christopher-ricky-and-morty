8c757db2f2a3cad39de9b3fe0e63debc3257f9ec
chris-to-fer <mail@christopherhaug.de>
2024-01-10 22:17:24 +0100

    Buttons and pagination as external modules

--- components/nav-pagination/nav-pagination.js
+++ components/nav-pagination/nav-pagination.js
@@ -1,6 +1,16 @@
-import { pagination } from "../../index.js";
-import { page } from "../nav-button/nav-button.js";
+import { page } from "../../index.js";
 import { maxPage } from "../../index.js";
+import { pagination } from "../../index.js";
+import { navigation } from "../../index.js";
+
+export function createPagination() {
+  const pagination = document.createElement("span");
+  pagination.innerHTML = `
+  <span class="navigation__pagination" data-js="pagination"></span>
+  `;
+  navigation.prepend(pagination);
+  return pagination;
+}
 
 export function pageDisplay() {
   pagination.textContent = `${page} / ${maxPage}`;
--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 import { createCharacterCard } from "./components/card/card.js";
-import { page } from "./components/nav-button/nav-button.js";
+import { createPrevButton } from "./components/nav-button/createButton.js";
+import { createNextButton } from "./components/nav-button/createButton.js";
 import { pageDisplay } from "./components/nav-pagination/nav-pagination.js";
+import { createPagination } from "./components/nav-pagination/nav-pagination.js";
 
-export const cardContainer = document.querySelector(
-  '[data-js="card-container"]'
-);
+const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
 );
 const searchBar = document.querySelector('[data-js="search-bar"]');
-const navigation = document.querySelector('[data-js="navigation"]');
-export const pagination = document.querySelector('[data-js="pagination"]');
+export const navigation = document.querySelector('[data-js="navigation"]');
 
 // States
 export let maxPage;
-
+export let page = 1;
 const searchQuery = "";
+
+export const pagination = createPagination();
 
 export async function fetchCharacters() {
   const URL = `https://rickandmortyapi.com/api/character/?page=${page}`;
@@ -30,3 +31,25 @@ export async function fetchCharacters() {
   });
 }
 fetchCharacters();
+
+createPrevButton(onClickPrev);
+createNextButton(onClickNext);
+
+//callback functions for eventListener in Button create
+export function onClickPrev() {
+  cardContainer.innerHTML = "";
+  if (page >= 2) {
+    page -= 1;
+  }
+  pageDisplay();
+  fetchCharacters();
+}
+
+export function onClickNext() {
+  cardContainer.innerHTML = "";
+  if (page < 42) {
+    page += 1;
+  }
+  pageDisplay();
+  fetchCharacters();
+}
