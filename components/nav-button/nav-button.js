dd0c9d0c49b2867fe7f302146df28f3b698ab364
chris-to-fer <mail@christopherhaug.de>
2024-01-10 16:33:02 +0100

    fixed button and switching pages

--- components/nav-button/nav-button.js
+++ components/nav-button/nav-button.js
@@ -1,17 +1,20 @@
 //import { prevButton } from "../../index.js";
 //import { nextButton } from "../../index.js";
+import { cardContainer } from "../../index.js";
 import { fetchCharacters } from "../../index.js";
 const prevButton = document.querySelector('[data-js="button-prev"]');
 const nextButton = document.querySelector('[data-js="button-next"]');
-export let page = 2;
+export let page = 1;
 prevButton.addEventListener("click", () => {
-  if (page > 2) {
+  cardContainer.innerHTML = "";
+  if (page >= 2) {
     page -= 1;
   }
 
   fetchCharacters();
 });
 nextButton.addEventListener("click", () => {
+  cardContainer.innerHTML = "";
   if (page < 42) {
     page += 1;
   }
