fece062f562af214bbaae0cd45d5ae5380a87805
chris-to-fer <mail@christopherhaug.de>
2024-01-10 14:45:38 +0100

    fetch done + page variable update

--- index.js
+++ index.js
@@ -12,17 +12,20 @@ const pagination = document.querySelector('[data-js="pagination"]');
 
 // States
 const maxPage = 1;
-const page = 1;
+let page = 1;
 const searchQuery = "";
 
 export async function fetchCharacters() {
-  const URL = "https://rickandmortyapi.com/api/character";
+  const URL = "https://rickandmortyapi.com/api/character/?page=2";
+  const urlArray = URL.split("");
+
+  page = urlArray[urlArray.length - 1];
+  console.log(page);
   const response = await fetch(URL);
   const data = await response.json();
   //console.log(data);
-
+  console.log(data);
   data.results.forEach((e) => {
-    console.log(data.results[data.results.indexOf(e)]);
     const card = createCharacterCard(data.results[data.results.indexOf(e)]);
 
     cardContainer.append(card);
