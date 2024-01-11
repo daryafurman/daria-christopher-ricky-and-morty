1adf1529b376c95e14f7bd9832dce511981e39c5
furmandarya1@gmail.com <furmandarya1@gmail.com>
2024-01-11 12:24:21 +0100

    search bar is working

--- components/nav-button/nav-button.js
+++ components/nav-button/nav-button.js
@@ -1,25 +1,24 @@
 //import { prevButton } from "../../index.js";
 //import { nextButton } from "../../index.js";
-import { cardContainer } from "../../index.js";
+import { cardContainer, maxPage } from "../../index.js";
 import { fetchCharacters } from "../../index.js";
 import { pageDisplay } from "../nav-pagination/nav-pagination.js";
 
 const prevButton = document.querySelector('[data-js="button-prev"]');
 const nextButton = document.querySelector('[data-js="button-next"]');
+
 export let page = 1;
 prevButton.addEventListener("click", () => {
-  cardContainer.innerHTML = "";
   if (page >= 2) {
     page -= 1;
   }
-  pageDisplay();
-  fetchCharacters();
 });
+
 nextButton.addEventListener("click", () => {
-  cardContainer.innerHTML = "";
-  if (page < 42) {
+  if (page < maxPage) {
     page += 1;
+    cardContainer.innerHTML = "";
+    pageDisplay();
+    fetchCharacters();
   }
-  pageDisplay();
-  fetchCharacters();
 });
