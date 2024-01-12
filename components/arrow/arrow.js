0cdfd6363830fadf2d0f7a1737af3bf1999ef362
chris-to-fer <mail@christopherhaug.de>
2024-01-12 10:46:22 +0100

    scroll change to center

--- components/arrow/arrow.js
+++ components/arrow/arrow.js
@@ -1,17 +1,10 @@
 const scrollButton = document.getElementById("scroll-btn");
 
 scrollButton.addEventListener("click", () => {
-  window.scrollTo({
-    top: document.body.scrollHeight,
-    behavior: "smooth",
-  });
-});
-
-scrollButton.addEventListener("click", () => {
   const scrollContainer = document.documentElement || document.body;
   const scrollPosition = scrollContainer.scrollTop;
 
-  if (scrollPosition > 0) {
+  if (scrollPosition > scrollContainer.scrollHeight / 2 - 500) {
     window.scrollTo({
       top: 0,
       behavior: "smooth",
